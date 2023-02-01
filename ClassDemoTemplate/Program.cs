7659d9daf9131d3333bb9cec10df3b7f0272265b
Peter Levinsky <PELE@zealand.dk>
2023-02-01 20:52:11 +0100

    Add project files.

---
+++ ClassDemoTemplate/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassDemoTemplate.template;
+
+Console.WriteLine("Hello, World!");
+
+AbstractADT adt = new ListADT();
+//AbstractADT adt = new SetADT();
+
+adt.Add("Peter");
+adt.Add("Anders");
+adt.Add("Jens Peter");
+adt.Add("Michael");
+adt.Add("Michael");
+
+Console.WriteLine(adt);
---
+++ ClassDemoTemplate/template/SetADT.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoTemplate.template
+{
+    public class SetADT : AbstractADT
+    {
+        protected override ICollection<string> CreateCollection()
+        {
+            return new HashSet<String>();
+        }
+    }
+}
